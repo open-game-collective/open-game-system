2f65cb2d4c83b7d91997e35b5a6b1d2103a726d0
Jonathan Mumm <jon@explorers.club>
2023-05-01 05:44:16 -0700

    test netlify ssr

--- apps/explorers-game-web/astro.config.mjs
+++ apps/explorers-game-web/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
 export default defineConfig({
   outDir: '../../dist/apps/explorers-game-web',
+  output: 'server',
   integrations: [react()],
+  adapter: netlify(),
 });
