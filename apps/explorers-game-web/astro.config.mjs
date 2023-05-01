e41ff0aa74d270b41e457778e002a09801a480fe
Jonathan Mumm <jon@explorers.club>
2023-05-01 05:16:20 -0700

    update nx and try nextjs

--- apps/explorers-game-web/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-export default defineConfig({
-  outDir: '../../dist/apps/explorers-game-web',
-  integrations: [react()],
-});
