11615491613b229502edad684e46da86ea7534d2
Jonathan Mumm <jon@explorers.club>
2023-05-01 15:58:39 -0700

    cleanup

--- apps/explorers-game-web/astro.config.mjs
+++ apps/explorers-game-web/astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
-const dist = new URL('../../dist/apps/explorers-game-web', import.meta.url);
-
 export default defineConfig({
   output: 'server',
   integrations: [react()],
-  adapter: netlify()
-  // adapter: netlify({
-  //   dist,
-  // }),
+  adapter: netlify(),
 });
