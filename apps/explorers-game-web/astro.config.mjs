a042b32919a0500d2754b10acf374d0df1adac05
Jonathan Mumm <jon@explorers.club>
2023-05-09 08:39:45 -0700

    add node adapter

--- apps/explorers-game-web/astro.config.mjs
+++ apps/explorers-game-web/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-// import netlify from '@astrojs/netlify/functions';
+import nodeAdapter from '@astrojs/adapter-node';
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
   integrations: [react()],
+  adapter: nodeAdapter(),
 });
