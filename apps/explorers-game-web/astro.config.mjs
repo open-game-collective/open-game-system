f48204f219a32ce8505775c39bf534ed9ab34540
Jonathan Mumm <jon@explorers.club>
2023-05-09 08:26:29 -0700

    dont build for netlify

--- apps/explorers-game-web/astro.config.mjs
+++ apps/explorers-game-web/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+// import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
   integrations: [react()],
-  adapter: netlify(),
 });
