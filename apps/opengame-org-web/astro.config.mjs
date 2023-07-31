0462c93efc9c351b310cfb79d5ed081e717ac136
Jonathan Mumm <jon@explorers.club>
2023-07-31 06:59:18 -0700

    setup for opengame org

--- apps/opengame-org-web/astro.config.mjs
+++ apps/opengame-org-web/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: '../../dist/apps/ogc-lol-web',
+  outDir: '../../dist/apps/opengames-org-web',
   experimental: {
     assets: true,
   },
