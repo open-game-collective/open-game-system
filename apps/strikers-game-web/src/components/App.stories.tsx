8d15ba787e5ac531d4a9f34f177c277d4de4f12a
Jonathan Mumm <jon@explorers.club>
2023-07-31 17:27:29 -0700

    use service urls for api

--- apps/strikers-game-web/src/components/App.stories.tsx
+++ apps/strikers-game-web/src/components/App.stories.tsx
@@ -14,8 +14,10 @@ import { ApplicationProvider } from '@context/ApplicationProvider';
 import { App } from './App';
 import { envSchema } from '../env.schema';
 
-const { PUBLIC_API_HTTP_SERVER_URL, PUBLIC_API_WS_SERVER_URL } =
-  envSchema.parse(process.env);
+const {
+  RAILWAY_SERVICE_APP_STRIKERS_GAME_WEB_URL,
+  RAILWAY_SERVICE_APP_API_SERVER_URL,
+} = envSchema.parse(process.env);
 
 const RoomWrapper: FC<{ slug: string }> = (props) => {
   const { slug } = props;
@@ -32,7 +34,7 @@ const RoomWrapper: FC<{ slug: string }> = (props) => {
       try {
         const creds = await initAccessToken(
           { name: 'Room', roomSlug: slug } satisfies RouteProps,
-          `${PUBLIC_API_HTTP_SERVER_URL}/${slug}`
+          `https://${RAILWAY_SERVICE_APP_STRIKERS_GAME_WEB_URL}/${slug}`
         );
         setCreds(creds);
       } catch (ex) {
@@ -47,7 +49,7 @@ const RoomWrapper: FC<{ slug: string }> = (props) => {
 
   return (
     <ApplicationProvider
-      trpcUrl={`${PUBLIC_API_WS_SERVER_URL}/?accessToken=${creds.accessToken}`}
+      trpcUrl={`ws://${RAILWAY_SERVICE_APP_API_SERVER_URL}/?accessToken=${creds.accessToken}`}
       initialRouteProps={routeProps}
       connectionId={creds.connectionId}
     >
@@ -204,11 +206,17 @@ const initAccessToken = async (routeProps: RouteProps, url: string) => {
 
   const accessToken = await jwt.sign(secret);
 
+  // If port specified, assume no http
+  const isHttps = !RAILWAY_SERVICE_APP_API_SERVER_URL.includes(':');
+  const apiUrl =
+    (isHttps ? 'https' : 'http') +
+    `://${RAILWAY_SERVICE_APP_API_SERVER_URL}/trpc`;
+
   const client = createTRPCProxyClient<ApiRouter>({
     transformer,
     links: [
       httpBatchLink({
-        url: `http://localhost:3001/trpc`,
+        url: apiUrl,
         // You can pass any HTTP headers you wish here
         async headers() {
           return {
--- apps/strikers-game-web/src/env.schema.ts
+++ apps/strikers-game-web/src/env.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
-  PUBLIC_API_HTTP_SERVER_URL: z.string(),
-  PUBLIC_API_WS_SERVER_URL: z.string(),
+  RAILWAY_SERVICE_APP_API_SERVER_URL: z.string(),
+  RAILWAY_SERVICE_APP_STRIKERS_GAME_WEB_URL: z.string(),
 });
