03c41dba827993b0f7e280e5fbe2a5fd7b9bddc3
Jonathan Mumm <jon@explorers.club>
2023-08-04 05:30:39 -0700

    ogc fixes

--- apps/opengame-org-web/astro.config.mjs
+++ apps/opengame-org-web/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: '../../dist/apps/opengames-org-web',
+  outDir: '../../dist/apps/opengame-org-web',
   experimental: {
     assets: true,
   },
