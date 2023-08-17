ccc0352e8e2f7870fed926e2ca3576d6ec352edd
Jonathan Mumm <jon@explorers.club>
2023-08-16 21:33:53 -0700

    add party town and update webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  integrations: [partytown()]
 });
