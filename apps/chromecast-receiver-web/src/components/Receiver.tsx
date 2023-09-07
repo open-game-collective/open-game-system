7d57de4fc27b8275116a228f7fef48bcbf24f0a0
Jonathan Mumm <jon@explorers.club>
2023-09-07 14:58:12 -0700

    add media player to receiver

--- apps/chromecast-receiver-web/src/components/Receiver.tsx
+++ apps/chromecast-receiver-web/src/components/Receiver.tsx
@@ -2,7 +2,9 @@ export default function Receiver() {
   const context = cast.framework.CastReceiverContext.getInstance();
   const playerManager = context.getPlayerManager();
 
-  context.start();
+  context.start({
+    disableIdleTimeout: true,
+  });
   console.log('HELLO!', context, playerManager);
   //   function addToCart(e: SubmitEvent) {
   //     e.preventDefault();
